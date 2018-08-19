a4f8eb478b9b46050816a78036963cef20a730a2
MelnikovRuslan <rulik1233@gmail.com>
2018-08-19 20:31:16 +0300

    Homework six

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
-let price = +prompt('Please enter price', '0');
-let discount = +prompt('Please enter discount', '0');
+let price = ~~((+prompt('Please enter price', '0')) * 100) / 100;
+let discount = ~~((+prompt('Please enter discount', '0')) * 100) / 100;
 let result = '';
 if(price > 0 && discount >= 0 && discount <=100 ){
 	result = ` Price without discount: ${price}\n Discount: ${discount}%\n`;
